2faaec8da12f7e1e5bb222fc99fdeaa57dec2ed3
perry <perry@accordiasolution.com>
2018-12-14 16:49:06 +0800

    Revised calculator logic

--- src/DtCalculatorService.php
+++ src/DtCalculatorService.php
@@ -3,6 +3,7 @@
 namespace Prymag\DtCalculator;
 
 use \Carbon\Carbon;
+use Illuminate\Support\Collection;
 
 class DtCalculatorService {
 
@@ -14,6 +15,16 @@ class DtCalculatorService {
 
     protected $the_date;
 
+    protected $start_date;
+
+    protected $end_date;
+
+    protected $days_to_skip;
+
+    protected $working_days;
+
+    protected $timezone = 'UTC';
+
     public function __construct(Carbon $carbon)
     {
         $this->carbon = $carbon;
@@ -22,13 +33,49 @@ class DtCalculatorService {
     public function setDate(Carbon $date)
     {
         $this->the_date = $date;
+        $this->start_date = $this->the_date->copy();
+
+        if ($this->days_to_skip) {
+            $this->skipDates($this->days_to_skip);
+        }
 
         return $this;
     }
 
-    public function getTheDate()
+    public function setWorkingDays(Collection $working_days)
+    {
+        $this->working_days = $working_days;
+
+        return $this;
+    }
+
+    public function getDate()
     {
         return $this->the_date;
+    }
+
+    public function getStartDate()
+    {
+        // Always start at the next day for the start
+        return $this->start_date
+            ->copy()
+            ->addDay()
+            ->startOfDay();
+    }
+
+    public function getEndDate()
+    {
+        return $this->the_date->copy()->endOfDay();
+    }
+
+    public function setTimezone($timezone)
+    {
+        $this->timezone = $timezone;
+    }
+
+    public function getTimezone()
+    {
+        return $this->timezone;
     }
 
     public function add($int)
@@ -69,9 +116,205 @@ class DtCalculatorService {
         return $this->interval;
     }
 
-    public function compute()
+    public function skipDates(Collection $dates)
     {
-        return $this->int * 3;
+        //
+        if ($dates->count()) {
+            //
+            $this->days_to_skip = $dates->sort();
+        }
+        
+        return $this;
     }
 
+    public function compute()
+    {
+        $this->addInterval();
+
+        $this->processDates();
+        //$this->processSkippableDates();
+
+        return $this->the_date;
+    }
+
+    public function processDates()
+    {
+        $this->adjustToWorkingDay();
+
+        $start = $this->getStartDate();
+        $end = $this->getEndDate();
+        
+        $periods = \Carbon\CarbonPeriod::create($start, '1 day', $end);
+        
+        $ctr = 0;
+        foreach ($periods as $date) {
+            if ($this->isDateSkippable($date)) {
+                $ctr++;
+            }
+        }
+        
+        if ($ctr) {
+            $this->int = $ctr;
+            $this->start_date = $this->the_date->copy();
+            $this->compute();
+        }
+    }
+
+    public function isDateSkippable($date)
+    {
+        if ($this->working_days && !$this->working_days[$date->format('w')]['enabled']) {
+            return true;
+        }
+        
+        if (!empty($this->days_to_skip) && $this->days_to_skip->count()) {
+            foreach ($this->days_to_skip as $skip_date) {
+                //
+                $start_day = $skip_date->copy()->startOfDay();
+                $end_day = $skip_date->copy()->endOfDay();
+                //
+                if (
+                    $date->greaterThanOrEqualTo($start_day) &&
+                    $date->lessThanOrEqualTo($end_day)
+                ) {
+                    return true;
+                    break;
+                }
+            }
+        }
+    }
+
+    public function adjustToWorkingDay()
+    {
+        if ($this->working_days && $this->working_days[$this->the_date->format('w')]['enabled']) {
+            $working_day = $this->working_days[$this->the_date->format('w')];
+            $format = 'Y-m-d H:i';
+            $date_start = $this->the_date->format('Y') . '-' . $this->the_date->format('m') . '-' . $this->the_date->format('d') . ' ' . $working_day['start'];
+            $date_end = $this->the_date->format('Y') . '-' . $this->the_date->format('m') . '-' . $this->the_date->format('d') . ' ' . $working_day['end'];
+            
+            $working_day_start = \Carbon\Carbon::createFromFormat($format, $date_start);
+            $working_day_end = \Carbon\Carbon::createFromFormat($format, $date_end);
+
+            if ($this->the_date->lessThan($working_day_start)) {
+                $this->the_date->setTime($working_day_start->format('H'), $working_day_start->format('i'), $working_day_start->format('s'));
+            }
+
+            if ($this->the_date->greaterThan($working_day_end)) {
+                // Get the next working day
+                $next_working_day = $this->getNextWorkingDay($this->the_date);
+                $next_working_date = $this->the_date->format('Y') . '-' . $this->the_date->format('m') . '-' . $this->the_date->format('d') . ' ' . $next_working_day['start'];
+                $next_working_date = \Carbon\Carbon::createFromFormat($format, $next_working_date);
+                $this->the_date->addDay();
+                $this->the_date->setTime($next_working_date->format('H'), $next_working_date->format('i'), $next_working_date->format('s'));
+            }
+        }
+    }
+
+    /* public function processSkippableDates()
+    {
+        if (!empty($this->days_to_skip) && $this->days_to_skip->count()) {
+            //
+            $days_to_skip = $this->days_to_skip;
+            $start = $this->getStartDate();
+            //
+            foreach ($days_to_skip as $k => $date) {
+                // Remove dates that are less than the start date
+                // and move to next loop
+                if ($date->lessThan($this->start_date)) {
+                    unset($days_to_skip[$k]);
+                    continue;
+                }
+                //
+                $end = $this->getEndDate();
+                // Always assume that the skippable dates are sorted in ascending order
+                // Check if date in skippable is within range
+                if ($date->greaterThan($start) && $date->lessThan($end)) {
+                    $this->addInterval(1);
+                    $this->processNonWorkingDates();
+                }
+
+                unset($days_to_skip[$k]);
+            }
+            return;
+        }
+
+        $this->processNonWorkingDates();
+    } */
+
+    /* public function processNonWorkingDates()
+    {
+        if ($this->working_days) {
+            $working_days = $this->working_days;
+            $start = $this->getStartDate();
+            $end = $this->getEndDate();
+            $periods = \Carbon\CarbonPeriod::create($start, '1 day', $end);
+
+            // Check individual dates if 
+            foreach ($periods as $period) {
+                if (!$working_days[$period->format('w')]['enabled']) {
+                    $this->addInterval(1);
+                }
+            }
+            while (!$working_days[$end->format('N')]['enabled']) {
+                $this->addInterval(1);
+                
+                $end = $this->getEndDate();
+            }
+            // Check the time of the working day
+            $working_day = $working_days[$end->format('N')];
+            $format = 'Y-m-d H:i';
+            $date_start = $end->format('Y') . '-' . $end->format('m') . '-' . $end->format('d') . ' ' . $working_day['start'];
+            $date_end = $end->format('Y') . '-' . $end->format('m') . '-' . $end->format('d') . ' ' . $working_day['end'];
+            
+            $working_day_start = \Carbon\Carbon::createFromFormat($format, $date_start);
+            $working_day_end = \Carbon\Carbon::createFromFormat($format, $date_end);
+
+            if ($this->the_date->lessThan($working_day_start)) {
+                $this->the_date->setTime($working_day_start->format('H'), $working_day_start->format('i'), $working_day_start->format('s'));
+            }
+            
+            if ($this->the_date->greaterThan($working_day_end)) {
+                // Get the next working day
+                $next_working_day = $this->getNextWorkingDay($this->the_date);
+                
+                // @todo here.
+                $this->the_date->setTime($next_working_day->format('H'), $next_working_day->format('i'), $next_working_day->format('s'));
+            }
+        }
+    } */
+    
+    public function getNextWorkingDay($date)
+    {
+        $index = $date->format('N') + 1;
+        $index = $index > 6 ? 0 : $index;
+        return $this->working_days[$index]['enabled'] ?
+            $this->working_days[$index] :
+            $this->getNextWorkingDay($date->copy()->addDay());
+    }
+
+    public function addInterval($inc = false)
+    {
+        $interval = 'add' . ucfirst($this->interval);
+        $int = $inc ? $inc : $this->int;
+        $this->the_date->$interval($int);
+    }
+
+    public function addDay()
+    {
+        $this->the_date->addDay();
+    }
+
+    public function isDateToBeSkipped()
+    {
+        if (empty($this->days_to_skip)) 
+            return false;
+        
+        foreach($this->days_to_skip as $k => $day) {
+            if ($this->the_date->format('Y-m-d') == $day->format('Y-m-d')) {
+                //unset($this->days_to_skip[$k]);
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
--- tests/DemoTest.php
+++ tests/DemoTest.php
@@ -24,8 +24,102 @@ class DemoTest extends TestCase
         $this->assertEquals($interval, 'days');
     }
 
-    public function testCanSetTheDate()
+    public function testCanAddDay()
     {
-        dd($this->dt_calculator->setDate(\Carbon\Carbon::now()));
+        $date = \Carbon\Carbon::create(2018, 8, 25, 3, 23);
+
+        $result = $this->dt_calculator->setDate($date)->add(3)->days()->compute();
+        
+        $str = $result->format('Y-m-d h:i A');
+        
+        $this->assertEquals($str, '2018-08-28 03:23 AM');
+    }
+
+    public function testCanSkipDays()
+    {
+        $date = \Carbon\Carbon::create(2018, 8, 25, 3, 23);
+
+        $dates = collect([
+            \Carbon\Carbon::create(2018, 8, 28),
+            \Carbon\Carbon::create(2018, 8, 26),
+            \Carbon\Carbon::create(2018, 8, 25),
+            \Carbon\Carbon::create(2018, 8, 27),
+            \Carbon\Carbon::create(2018, 8, 31),
+            \Carbon\Carbon::create(2018, 9, 2),
+            \Carbon\Carbon::create(2018, 9, 3),
+        ]);
+
+        $result = $this->dt_calculator
+            ->setDate($date)
+            ->add(3)
+            ->days()
+            ->skipDates($dates)
+            ->compute();
+
+        $str = $result->format('Y-m-d h:i A');
+        
+        $this->assertEquals($str, '2018-09-01 03:23 AM');
+    }
+
+    public function testCanSkipWorkingDays()
+    {
+        $date = \Carbon\Carbon::create(2018, 12, 14, 15, 23);
+
+        $working_days = collect([
+            '0' => ['enabled' => false, 'start' => '08:30', 'end' => '14:30'], // Sunday
+            '1' => ['enabled' => true, 'start' => '08:30', 'end' => '14:30'], // Monday
+            '2' => ['enabled' => true, 'start' => '12:30', 'end' => '14:30'], // Tuesday
+            '3' => ['enabled' => false, 'start' => '08:30', 'end' => '14:30'], // Wednesday
+            '4' => ['enabled' => false, 'start' => '08:30', 'end' => '14:30'], // Thursday
+            '5' => ['enabled' => true, 'start' => '09:30', 'end' => '14:30'], // Friday
+            '6' => ['enabled' => false, 'start' => '08:30', 'end' => '14:30'], // Saturday
+        ]);
+
+        $result = $this->dt_calculator
+            ->setDate($date)
+            ->setWorkingDays($working_days)
+            ->add(7)
+            ->days()
+            ->compute();
+
+        $str = $result->format('Y-m-d h:i A');
+        $this->assertEquals('2019-01-01 12:30 PM', $str);
+    }
+
+    public function testcanSkipWorkingDaysAndSkipDates()
+    {
+        $date = \Carbon\Carbon::create(2018, 12, 25, 5, 26);
+
+        $dates = collect([
+            \Carbon\Carbon::create(2018, 12, 28),
+            \Carbon\Carbon::create(2018, 12, 26),
+            \Carbon\Carbon::create(2018, 12, 25),
+            \Carbon\Carbon::create(2018, 12, 27),
+            \Carbon\Carbon::create(2018, 12, 31),
+            \Carbon\Carbon::create(2018, 12, 2),
+            \Carbon\Carbon::create(2018, 12, 3),
+        ]);
+
+        $working_days = collect([
+            '0' => ['enabled' => false, 'start' => '08:30', 'end' => '14:30'], // Sunday
+            '1' => ['enabled' => false, 'start' => '08:30', 'end' => '14:30'], // Monday
+            '2' => ['enabled' => true, 'start' => '08:30', 'end' => '14:30'], // Tuesday
+            '3' => ['enabled' => true, 'start' => '08:30', 'end' => '14:30'], // Wednesday
+            '4' => ['enabled' => true, 'start' => '08:30', 'end' => '14:30'], // Thursday
+            '5' => ['enabled' => true, 'start' => '08:30', 'end' => '14:30'], // Friday
+            '6' => ['enabled' => true, 'start' => '08:30', 'end' => '14:30'], // Saturday
+        ]);
+
+        $result = $this->dt_calculator
+            ->setDate($date)
+            ->setWorkingDays($working_days)
+            ->add(3)
+            ->days()
+            ->skipDates($dates)
+            ->compute();
+
+        $str = $result->format('Y-m-d h:i A');
+        
+        $this->assertEquals($str, '2018-09-01 03:23 AM');
     }
 }
